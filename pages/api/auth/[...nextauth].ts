76226a0f5d90e361a7f2f46c77779f26e1a33807
Alfred Melson <vaughnmelson@outlook.com>
2022-10-19 08:00:13 +0300

    type saftey

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,13 +2,14 @@ import NextAuth from 'next-auth';
 import GitHubProvider from 'next-auth/providers/github';
 import { MongoDBAdapter } from '@next-auth/mongodb-adapter';
 import clientPromise from 'lib/mongodb';
+import { env } from '../../../env/server.mjs';
 
 export default NextAuth({
   adapter: MongoDBAdapter(clientPromise),
   providers: [
     GitHubProvider({
-      clientId: process.env.GITHUB_CLIENT_ID,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      clientId: env.GITHUB_CLIENT_ID,
+      clientSecret: env.GITHUB_CLIENT_SECRET,
       profile(profile) {
         return {
           id: profile.id.toString(),
@@ -25,7 +26,7 @@ export default NextAuth({
   callbacks: {
     async session({ session, user }) {
       // Send properties to the client, like an access_token from a provider.
-      session.username = user.username;
+      session.user = user;
       return session;
     }
   }
